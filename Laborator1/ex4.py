a0e7270dba5a1656c9997b502f9da9e9ef42117e
TOMA Victor-Stefan <viky.toma15@gmail.com>
2023-10-11 17:26:42 +0300

    Done Laboratory1

--- Laborator1/ex4.py
+++ Laborator1/ex4.py
@@ -6,4 +6,4 @@ for index in input_string:
         output_string = output_string + index.lower()
     else:
         output_string = output_string + "_" + index.lower()
-print(output_string)
+print(output_string[1:])
